ef48182683eae4afd6205cd73dffc9f1334763ff
StriderCraft315 <michaelmj2540@gmail.com>
2026-02-09 09:15:28 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,18 +12,17 @@
 EOF
 
 
-echo "Applying Docker Permissions to vps-1421860082894766183-1 "
+echo "Applying Docker Permissions to atherixcloud-vps-1421860082894766183-1 "
 
-lxc config set vps-1421860082894766183-1 security.nesting true
-lxc config device add vps-1421860082894766183-1 kvm unix-char path=/dev/kvm
-lxc config set vps-1421860082894766183-1 security.nesting true
-lxc config set vps-1421860082894766183-1 security.privileged true
-lxc config set vps-1421860082894766183-1 security.syscalls.intercept.mknod true
-lxc config set vps-1421860082894766183-1 security.syscalls.intercept.setxattr true
-lxc config device add vps-1421860082894766183-1 fuse unix-char path=/dev/fuse
-lxc config set vps-1421860082894766183-1 linux.kernel_modules overlay,loop,nf_nat,ip_tables,ip6_tables,netlink_diag,br_netfilter
-lxc restart vps-1421860082894766183-1
-
+lxc config set atherixcloud-vps-1421860082894766183-1 security.nesting true
+lxc config device add atherixcloud-vps-1421860082894766183-1 kvm unix-char path=/dev/kvm
+lxc config set atherixcloud-vps-1421860082894766183-1 security.nesting true
+lxc config set atherixcloud-vps-1421860082894766183-1 security.privileged true
+lxc config set atherixcloud-vps-1421860082894766183-1 security.syscalls.intercept.mknod true
+lxc config set atherixcloud-vps-1421860082894766183-1 security.syscalls.intercept.setxattr true
+lxc config device add atherixcloud-vps-1421860082894766183-1 fuse unix-char path=/dev/fuse
+lxc config set atherixcloud-vps-1421860082894766183-1 linux.kernel_modules overlay,loop,nf_nat,ip_tables,ip6_tables,netlink_diag,br_netfilter
+lxc restart atherixcloud-vps-1421860082894766183-1
 
 echo "All Done!! "
 echo "Enjoy The Power Of Docker "
